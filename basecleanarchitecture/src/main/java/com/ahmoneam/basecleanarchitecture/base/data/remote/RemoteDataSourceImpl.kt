04e1f4f6226ffe2fcf658e386ce172cd02a805d1
Ahmed Abdelfattah <ahmoneam@gmail.com>
2019-10-10 18:43:22 +0200

    - make RemoteDataSourceImpl open

--- basecleanarchitecture/src/main/java/com/ahmoneam/basecleanarchitecture/base/data/remote/RemoteDataSourceImpl.kt
+++ basecleanarchitecture/src/main/java/com/ahmoneam/basecleanarchitecture/base/data/remote/RemoteDataSourceImpl.kt
@@ -3,4 +3,4 @@ package com.ahmoneam.basecleanarchitecture.base.data.remote
 import retrofit2.Retrofit
 
 
-class RemoteDataSourceImpl constructor(retrofit: Retrofit) : RemoteDataSource
+open class RemoteDataSourceImpl constructor(retrofit: Retrofit) : RemoteDataSource
