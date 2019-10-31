9426d816e1fc56545d3d98b67d72baffacfbc671
Ahmed Abdelfattah <ahmoneam@gmail.com>
2019-10-31 13:36:48 +0200

    - implement admin login - implement cashier list

--- basecleanarchitecture/src/main/java/com/ahmoneam/basecleanarchitecture/base/platform/BaseNavigationDestination.kt
+++ basecleanarchitecture/src/main/java/com/ahmoneam/basecleanarchitecture/base/platform/BaseNavigationDestination.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import androidx.fragment.app.Fragment
 
 sealed class BaseNavigationDestination<T, Args>(open val finish: Boolean, open val args: Args) {
-    abstract fun start(com: T)
+    abstract fun start(component: T)
 
     abstract class Activities<Args>(finish: Boolean, args: Args) :
         BaseNavigationDestination<Activity, Args>(finish, args)
